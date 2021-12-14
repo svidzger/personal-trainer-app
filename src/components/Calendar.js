5bd17e669e53efa9b9b7974a1f4f65cfbfc2c1d3
svidzger <german.svidzinskij@gmail.com>
2021-12-14 19:38:21 +0200

    2 spaces fix

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -6,40 +6,40 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 const localizer = momentLocalizer(moment);
 
 function TrainingCalendar() {
-	const [trainings, setTrainings] = useState([]);
+  const [trainings, setTrainings] = useState([]);
 
-    useEffect(() => {
-        fetchTrainings();
-    }, [])
+  useEffect(() => {
+    fetchTrainings();
+  }, [])
 
-    const fetchTrainings = () => {
-        fetch('https://customerrest.herokuapp.com/gettrainings')
-        .then(response => response.json())
-        .then(data => setTrainings(data))
-        .catch(err => console.error(err))
-    };
+  const fetchTrainings = () => {
+    fetch('https://customerrest.herokuapp.com/gettrainings')
+      .then(response => response.json())
+      .then(data => setTrainings(data))
+      .catch(err => console.error(err))
+  };
 
-    const myEventsList = trainings.map(training => {
-        let date = new Date(training.date)
-        const eventsDetails = {
-            start: new Date(training.date),
-            end: new Date(moment(date).add(training.duration, "minutes")),
-            title: training.activity + '/' + training.customer.firstname + ' ' + training.customer.lastname
-        }
-        return eventsDetails
-	});
+  const myEventsList = trainings.map(training => {
+    let date = new Date(training.date)
+    const eventsDetails = {
+      start: new Date(training.date),
+      end: new Date(moment(date).add(training.duration, "minutes")),
+      title: training.activity + '/' + training.customer.firstname + ' ' + training.customer.lastname
+    }
+    return eventsDetails
+  });
 
-    return (
-        <div>
-            <Calendar
-                localizer={localizer}
-                events={myEventsList}
-                startAccessor="start"
-                endAccessor="end"
-                style={{marginTop: 100, marginLeft: 500, height: 1000, width: 1500, }}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ marginTop: 100, marginLeft: 500, height: 1000, width: 1500, }}
+      />
+    </div>
+  );
 }
 
 export default TrainingCalendar;
